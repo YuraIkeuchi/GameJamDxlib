9e74089a4218a655e221500d1c572bd0f5acc5fb
fuuma1223 <k020g1170@g.neec.ac.jp>
2022-09-06 11:01:02 +0900

    全体のクラス分け（関数分け） シーン切り替え（わかりずらいけどNキーを押すと切り替わる）

---
+++ SceneManager.h
@@ -0,0 +1,65 @@
+#pragma once
+#include"Player.h"
+#include"Enemy.h"
+#include"StageCircle.h"
+#include "math.h"
+#include<time.h>
+#include "DxLib.h"
+
+class SceneManager
+{
+private:
+	//固定値
+	static const int Enemy_Max = 5;
+private:
+	//シーンの種類
+	enum class NO
+	{
+		Title,
+		GameScene,
+		End,
+	};
+
+	//現在のシーンの番号
+	int SceneNo = static_cast<int>(NO::GameScene);
+	//初期化のための計測
+	int SceneTime = 0;
+#pragma region シーンごとの処理
+	void TitleInit();
+	void TitleUpdate(char keys[255], char oldkeys[255]);
+	void TitleDraw();
+
+	void GameSceneInit();
+	void GameSceneUpdate(char keys[255], char oldkeys[255]);
+	void GameSceneDraw();
+
+	void EndInit();
+	void EndUpdate(char keys[255], char oldkeys[255]);
+	void EndDraw();
+
+	//クラス宣言
+	// 	//エネミー
+	Enemy *enemy[Enemy_Max];
+	//プレイヤー
+	Player *player = nullptr;
+	//ステージ上の円
+	StageCircle *stagecircle = nullptr;
+
+	float x = WIN_WIDTH / 2;
+	float y = WIN_HEIGHT / 2;
+
+
+public:
+	//最初に初期化するもの
+	void StaticInit();
+	//シーンを切り替えるたびに初期化するもの
+	void Init();
+	//Update
+	void Update(char keys[255], char oldkeys[255]);
+	//Draw
+	void Draw();
+
+public:
+	//getter
+	const int &GetSceneTime() { return SceneTime; }
+};
